eb820d26a00020b104a896cac5ecfc8a03f1c743
orientiered <da.morgachev@mail.ru>
2024-12-16 19:32:32 +0300

    Added argv processing

--- Backend/source/main.c
+++ Backend/source/main.c
@@ -2,17 +2,61 @@
 #include <stdio.h>
 
 #include "logger.h"
+#include "argvProcessor.h"
 #include "utils.h"
 #include "nameTable.h"
 #include "backend.h"
 
-int main() {
+
+const int ARGV_EXIT_CODE = 3;
+const int NO_FILE_EXIT_CODE = 4;
+
+const size_t DEFAULT_MAX_TOKENS     = 1024;
+const size_t DEFAULT_NAMETABLE_SIZE = 256;
+const size_t DEFAULT_NAMES_LEN      = 2048;
+
+int main(int argc, const char *argv[]) {
     logOpen("log.html", L_HTML_MODE);
     setLogLevel(L_EXTRA);
     logDisableBuffering();
 
+    registerFlag(TYPE_STRING, "-i", "--input",  "Input file");
+    registerFlag(TYPE_STRING, "-o", "--output", "Output file");
+
+    registerFlag(TYPE_INT,    "-t", "--tokens", "Maximum number of tokens");
+    registerFlag(TYPE_INT,    "-n", "--nameTableSize", "Maximum number of records in nametable");
+    registerFlag(TYPE_INT,    "-l", "--namesLen", "Maximum total length of all names in nametable");
+
+    enableHelpFlag("Money language backend: transform IR files to SPU asm\n");
+
+    if (processArgs(argc, argv) != ARGV_SUCCESS) {
+        return ARGV_EXIT_CODE;
+    }
+
+    const char *inputFileName = getDefaultArgument(0);
+    if (!inputFileName) inputFileName = getFlagValue("-i").string_;
+    if (!inputFileName) {
+        logPrint(L_ZERO, 1, "No input file specified\n");
+        return NO_FILE_EXIT_CODE;
+    }
+
+    const char *outputFileName = getFlagValue("-o").string_;
+    if (!outputFileName) {
+        logPrint(L_ZERO, 1, "No output file specified\n");
+        return NO_FILE_EXIT_CODE;
+    }
+
+    size_t maxTokens = getFlagValue("-t").int_;
+    if (maxTokens == 0) maxTokens = DEFAULT_MAX_TOKENS;
+
+    size_t nameTableSize = getFlagValue("-n").int_;
+    if (nameTableSize == 0) nameTableSize = DEFAULT_NAMETABLE_SIZE;
+
+    size_t namesLen = getFlagValue("-l").int_;
+    if (namesLen == 0) namesLen = DEFAULT_NAMES_LEN;
+
     LangContext_t context = {0};
-    BackendInit(&context, "../out.ast", "../compiled.asm", 1024, 128, 1024);
+    BackendInit(&context, inputFileName, outputFileName, maxTokens, nameTableSize, namesLen);
 
     BackendRun(&context);
 
--- Frontend/source/main.c
+++ Frontend/source/main.c
@@ -2,25 +2,65 @@
 #include <stdio.h>
 
 #include "logger.h"
+#include "argvProcessor.h"
 #include "utils.h"
 #include "nameTable.h"
 #include "frontend.h"
 
-int main() {
+const int ARGV_EXIT_CODE    = 3;
+const int NO_FILE_EXIT_CODE = 4;
+
+const size_t DEFAULT_MAX_TOKENS     = 1024;
+const size_t DEFAULT_NAMETABLE_SIZE = 256;
+const size_t DEFAULT_NAMES_LEN      = 2048;
+
+int main(int argc, const char *argv[]) {
     logOpen("log.html", L_HTML_MODE);
     setLogLevel(L_EXTRA);
     logDisableBuffering();
 
+    registerFlag(TYPE_BLANK,  "-1", "-1",       "Translate IR to program");
+    registerFlag(TYPE_STRING, "-i", "--input",  "Input file");
+    registerFlag(TYPE_STRING, "-o", "--output", "Output file");
+
+    registerFlag(TYPE_INT,    "-t", "--tokens", "Maximum number of tokens");
+    registerFlag(TYPE_INT,    "-n", "--nameTableSize", "Maximum number of records in nametable");
+    registerFlag(TYPE_INT,    "-l", "--namesLen", "Maximum total length of all names in nametable");
+
+    enableHelpFlag("Money language frontend: transform program files to intermediate representation\n");
+
+    if (processArgs(argc, argv) != ARGV_SUCCESS) {
+        return ARGV_EXIT_CODE;
+    }
+
+    const char *inputFileName = getDefaultArgument(0);
+    if (!inputFileName) inputFileName = getFlagValue("-i").string_;
+    if (!inputFileName) {
+        logPrint(L_ZERO, 1, "No input file specified\n");
+        return NO_FILE_EXIT_CODE;
+    }
+
+    const char *outputFileName = getFlagValue("-o").string_;
+    if (!outputFileName) {
+        logPrint(L_ZERO, 1, "No output file specified\n");
+        return NO_FILE_EXIT_CODE;
+    }
+
+    enum FrontendMode_t mode = isFlagSet("-1") ? FRONTEND_BACKWARD : FRONTEND_FORWARD;
+
+    size_t maxTokens = getFlagValue("-t").int_;
+    if (maxTokens == 0) maxTokens = DEFAULT_MAX_TOKENS;
+
+    size_t nameTableSize = getFlagValue("-n").int_;
+    if (nameTableSize == 0) nameTableSize = DEFAULT_NAMETABLE_SIZE;
+
+    size_t namesLen = getFlagValue("-l").int_;
+    if (namesLen == 0) namesLen = DEFAULT_NAMES_LEN;
+
     LangContext_t context = {0};
-    FrontendInit(&context, "test.mpp", "out.ast", 1024, 128, 1024, FRONTEND_FORWARD);
+    FrontendInit(&context, inputFileName, outputFileName, maxTokens, nameTableSize, namesLen, mode);
 
-    programToTree(&context);
-
-    FrontendDelete(&context);
-
-    FrontendInit(&context, "out.ast", "revTest.mpp", 1024, 128, 1024, FRONTEND_BACKWARD);
-
-    treeToProgram(&context);
+    frontendRun(&context);
 
     FrontendDelete(&context);
 
